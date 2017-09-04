704bed57829eab28804785c5c380c0d8c2fd4b62
zhijund <762718879@qq.com>
2017-09-04 08:13:25 +0800

    添加基础模块代码

--- src/main/java/com/jund/base/entity/MenuEntity.java
+++ src/main/java/com/jund/base/entity/MenuEntity.java
@@ -1,7 +1,33 @@
 package com.jund.base.entity;
 
+import com.jund.framework.jpa.base.BaseEntity;
+
+import java.io.Serializable;
+
 /**
  * Created by zhijund on 2017/9/2.
  */
-public class MenuEntity {
+public class MenuEntity extends BaseEntity implements Serializable, Cloneable {
+
+    public MenuEntity clone() {
+        MenuEntity cloneMenu = null;
+        try {
+            cloneMenu = (MenuEntity) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return cloneMenu;
+    }
+
+    public String toString() {
+        return null;
+    }
+
+    public boolean equals(Object o) {
+        return false;
+    }
+
+    public int hashCode() {
+        return 0;
+    }
 }
